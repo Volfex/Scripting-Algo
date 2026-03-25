fb87bd43b84bb7c92d854148c1e0b6b6b85bbe42
Volfex <86154698+Volfex@users.noreply.github.com>
2026-03-25 21:46:17 +0100

    Add files via upload

---
+++ 3D Platformer/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int health = 10;
+    public AudioSource audio;
+    public AudioClip damageSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void getDamage(int damage)
+    {
+        health -= damage;
+        print("Životy:" + health);
+        
+        if (health < 1)
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+        else
+        {
+            audio.PlayOneShot(damageSound);
+        }
+        
+    }
+}
